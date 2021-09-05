84864db797ec1e17b8141deaf5366b037ac348b7
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-09-05 09:20:02 +0400

    Update and rename indeed_schedule.sh to indeed_download.sh

---
+++ indeed_download.sh
@@ -0,0 +1,6 @@
+####indeed_download.sh####
+while true; do
+   python3 indeed_download.py
+   sleep $[60 * 60]
+done
+####indeed_download.sh####
--- indeed_schedule.sh
+++
@@ -1,6 +0,0 @@
-####indeed_schedule.sh####
-while true; do
-   python3 indeed.py
-   sleep $[60 * 30]
-done
-####indeed_schedule.sh####
