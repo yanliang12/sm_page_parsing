5ca6a298490ae26d6783ff03b118bc816b0bb936
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-10-07 09:26:52 +0400

    Update khaleejtimes_download.sh

--- khaleejtimes_download.sh
+++ khaleejtimes_download.sh
@@ -1,6 +1,6 @@
 ####khaleejtimes_download.sh####
 while true; do
-   python3 khaleejtimes_download.py &
+   python3 khaleejtimes_download.py
    sleep $[60 * 60]
 done
 ####khaleejtimes_download.sh####
