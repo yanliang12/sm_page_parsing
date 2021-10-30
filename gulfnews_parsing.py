8f51f101e64b79458a30c794177b3bb50034be36
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-10-30 22:45:47 +0400

    Add files via upload

---
+++ gulfnews_parsing.py
@@ -0,0 +1,65 @@
+###########gulfnews_parsing.py###########
+import re
+import pandas
+
+##########
+
+re_news_attributes = [
+	re.compile(r'class\="image\-caption" itemprop\="description"\>\s*(?P<news__news_image_capition__text>[^\<\>]*?)\s*\<', flags=re.DOTALL),
+	re.compile(r'\<div class\=\"story\-block\"\>(?P<news__news_body_text>.*?)\<\/div\>\s*\<\/div\>', flags=re.DOTALL),
+	re.compile(r'\<meta name\="description" content\="(?P<news__news_description__description>[^\"]*?)"\s*\/\>', flags=re.DOTALL),
+	re.compile(r'\'publishedDate\'\:\s*\'(?P<news__news_publish_time__time>[^\']*?)\'\,', flags=re.DOTALL),
+	re.compile(r'\'page_section\'\:\s*\'(?P<news__news_category__news_category>[^\']*?)\'\,', flags=re.DOTALL),
+	re.compile(r'\'page_subsection\'\:\s*\'(?P<news__news_sub_category__news_sub_category>[^\']*?)\'\,', flags=re.DOTALL),
+	re.compile(r'\'title\'\:\s*\'(?P<news__news_title__title>[^\']*?)\',', flags=re.DOTALL),
+]
+
+re_author_block = re.compile(r'authors\'\:\s*\[[^\[\]]+\]\,', flags=re.DOTALL)
+
+re_author_attribute = [
+	re.compile(r'\"(?P<news__news_author_name__author_name>[^\"]*?)\"', flags=re.DOTALL),
+	]
+
+def page_parsing(
+	page_html, 
+	page_url,
+	):
+	output = []
+	for r in re_news_attributes:
+		for m in re.finditer(r, page_html):
+			output.append(m.groupdict())
+	for a in re.finditer(re_author_block, page_html):
+		b = a.group()
+		for r1 in re_author_attribute:
+			for m1 in re.finditer(r1, b):
+				output.append(m1.groupdict())
+	for e in output:
+		for k in e:
+			if k in ['news__news_body_text']:
+				e[k] = re.sub(r'\<[^\<\>]*?\>', r'', e[k])
+	return output
+
+##########
+
+'''
+page = pandas.read_json(
+	'/dcd_data/gulfnews/page_html/source=date2021/0bb82176789ac4ecc32b5bea83fec8e9.json',
+	lines = True,
+	orient = 'records')
+
+page_url = page['page_url'][0]
+page_html = page['page_html'][0]
+
+print(page_url)
+
+output = page_parsing(
+	page_html, 
+	page_url,
+	)
+
+for e in output:
+	print(e)
+
+'''
+
+###########gulfnews_parsing.py###########
--- khaleejtimes_parsing.py
+++ khaleejtimes_parsing.py
@@ -1,26 +1,54 @@
 ###########khaleejtimes_parsing.py###########
 import re
-import datetime
+import pandas
 
 ###########
 
-re_page_url = re.compile(r'\<h2\>\<a href\=\"(?P<page_url>[^\"]*?)\"\>', flags=re.DOTALL)
-page_url_prefix = 'https://www.khaleejtimes.com'
+re_news_attributes = [
+	re.compile(r'\<li\>\<a href\="\/"\>Home\<\/a\>\<\/li\>\s*\<li\>\/\<\/li\>\s*\<li class\="active"\>\<a href\="\/[^\"]*?"\>(?P<news__news_category__news_category>[^\<\>]*?)\<\/a\>\<\/li\>\s*\<li\>\/\<\/li\>\s*\<li class\="active"\>\<a href\="\/[^\"]*?"\>(?P<news__news_sub_category__news_sub_category>[^\<\>]*?)\<\/a\>\<\/li\>', flags=re.DOTALL),
+	re.compile(r'tags\-btm\-nf"\>\s*\<li\>\<a href\="[^\"]*"\>(?P<news__news_sub_category__news_sub_category>[^\<\>]*?)\<\/a\>\<\/li\>', flags=re.DOTALL),
+	re.compile(r'\<meta property\="og\:title" content\="(?P<news__news_title__title>[^\"]*?)"\/\>', flags=re.DOTALL),
+	re.compile(r'"description"\: "(?P<news__news_description__description>[^\"]*?)",', flags=re.DOTALL),
+	re.compile(r'by \<a href\="\/(?P<news__news_author_id__author_id>[^\"]*?)"\>\<h3\>(?P<news__news_author_name__author_name>[^\<\>]*?)\<\/h3\>\<\/a\>', flags=re.DOTALL),
+	re.compile(r'Published\:\<\/span\>\s*(?P<news__news_publish_time__time>[^\<\>]*?)\s*\<\/p\>', flags=re.DOTALL),
+	re.compile(r'class\="article\-lead\-img\-caption"\>\s*(?P<news__news_image_capition__text>[^\<\>]*?)\s*\<em', flags=re.DOTALL),
+	re.compile(r'\<\!\-\- element\: body \-\-\>(?P<news__news_body_text>.+)\<ul class\="tags\-btm\-nf"\>', flags=re.DOTALL),
+]
 
-def parsing_from_list_to_url(
-	page_html,
+
+def page_parsing(
+	page_html, 
 	page_url,
 	):
 	output = []
-	for m in re.finditer(
-		re_page_url,
-		page_html):
-		page_url1 = m.group('page_url')
-		page_url1 = '%s%s'%(page_url_prefix, page_url1)
-		output.append({'page_url':page_url1})
+	for r in re_news_attributes:
+		for m in re.finditer(r, page_html):
+			output.append(m.groupdict())
+	for e in output:
+		for k in e:
+			if k in ['news__news_body_text']:
+				e[k] = re.sub(r'\<[^\<\>]*?\>', r'', e[k])
 	return output
 
 ###########
+
+page = pandas.read_json(
+	'/dcd_data/khaleejtimes/page_html/source=date2021/9a7872071dcb7f60b5d74998c3bb9dde.json',
+	lines = True,
+	orient = 'records')
+
+page_url = page['page_url'][0]
+page_html = page['page_html'][0]
+
+print(page_url)
+
+output = page_parsing(
+	page_html, 
+	page_url,
+	)
+
+for e in output:
+	print(e)
 
 
 ###########khaleejtimes_parsing.py###########
