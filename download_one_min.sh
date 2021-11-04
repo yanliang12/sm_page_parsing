afd21e03ab34f27ec78a4711dcef51a260088466
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2021-11-04 23:21:46 +0400

    Create download_one_min.sh

---
+++ download_one_min.sh
@@ -0,0 +1,8 @@
+####download_one_min.sh####
+while true; do
+   python3 opensooq_download.py &
+   python3 bayut_download.py
+   python3 propertyfinder_download.py
+   sleep $[1 * 60]
+done
+####download_one_min.sh####
